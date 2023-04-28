abc8bf98c04ecadc393e735b6783a8c06038cf5a
alvesrenan90 <renan56220942@gmail.com>
2023-04-28 10:58:06 -0300

    correção script e POM

--- target/classes/db/script.sql
+++ target/classes/db/script.sql
@@ -1,99 +1,37 @@
-
-CREATE TABLE public.entidade (
-    id_entidade bigint NOT NULL,
-    cnpj character varying(20),
-    codigo integer,
-    esfera integer,
-    nome character varying(255),
-    poder integer,
-    id_entidade_brasao bigint,
-    codigo_municipio integer,
-    tipo_entidade character varying(64) NOT NULL,
-    mensageria_token character varying(255),
-    mensageria_id_cliente character varying(255),
-    nome_municipio character varying(255),
-    CONSTRAINT ocn_entidade_tipo_entidade_check CHECK (((tipo_entidade)::text = ANY (ARRAY[('PREFEITURA'::character varying)::text, ('CAMARA_MUNICIPAL'::character varying)::text, ('FUNDO_ESPECIAL'::character varying)::text, ('AUTARQUIA'::character varying)::text, ('FUNDACAO_PUBLICA'::character varying)::text, ('EMPRESA_PUBLICA_NAO_DEPENDENTE'::character varying)::text, ('SOCIEDADE_ECONOMIA_MISTA_NAO_DEPENDENTE'::character varying)::text, ('EMPRESA_PUBLICA_DEPENDENTE'::character varying)::text, ('SOCIEDADE_ECONOMIA_MISTA_DEPENDENTE'::character varying)::text, ('RPPS'::character varying)::text])))
+create table autor 
+(
+  id_autor integer(11) primary key NOT NULL,
+  nome varchar(120) NOT NULL,
+  sexo integer(30) NOT NULL,
+  email varchar (80) null,
+  data_nascimento varchar(20) null,
+  pais_origem varchar(80),
+  cpf varchar (11),
+  foreign key (fk_obra) references obra(id_obra)  
+);
+create table obra 
+(
+  id_obra integer(11) primary key NOT NULL,
+  descricao varchar(240) NOT NULL,
+  data_publicacao date NULL,
+  data_exposicao date NULL,  
+  foreign key(fk_autor) references autor(id_autor)
 );
 
-ALTER TABLE ONLY public.entidade
-    ADD CONSTRAINT mfc_entidade_pkey PRIMARY KEY (id_entidade);
+
+insert into obra (id_obra, descricao, data_publicacao, data_exposicao, id_autor) values (1, 'Vidas Secas', 'Nacional,' "2014-10-15", "2017-10-16", 4);
+insert into obra (id_obra, descricao, data_publicacao, data_exposicao, id_autor) values (2, 'A hora da estrela', 'Nacional', "2014-10-14", "2019-10-15", 2);
+insert into obra (id_obra, descricao, data_publicacao, data_exposicao, id_autor) values (3, 'O menino de asas', 'Nacional', "2018-10-14", "2020-10-17", 3);
+insert into obra (id_obra, descricao, data_publicacao, data_exposicao, id_autor) values (4, 'O código da Vinci', 'Internacional', "2010-10-14", "2012-10-15", 1);
+insert into obra (id_obra, descricao, data_publicacao, data_exposicao, id_autor) values (5, 'Desbloqueie o poder da sua mente', 'Nacional', "2021-10-14", "2022-10-15", 5);
+insert into obra (id_obra, descricao, data_publicacao, data_exposicao, id_autor) values (6, 'O escaravelho do diabo', 'Nacional', "2015-10-14", "2016-10-15", 4);
+insert into obra (id_obra, descricao, data_publicacao, data_exposicao, id_autor) values (7, 'A fortaleza', 'Internacional',"2014-10-14", "2015-11-15", 1);
+insert into obra (id_obra, descricao, data_publicacao, data_exposicao, id_autor) values (8, 'Pai rico e pai pobre', 'Internacional', "2014-11-14", "2019-10-15", 5);
 
 
-CREATE TABLE public.entidade_brasao (
-    id_entidade_brasao bigint NOT NULL,
-    brasao bytea,
-    mime_type character varying(100) NOT NULL
-);
-
-ALTER TABLE ONLY public.entidade_brasao
-    ADD CONSTRAINT infra_entidade_brasao_pkey PRIMARY KEY (id_entidade_brasao);
-
-
-CREATE TABLE public.fonte_recurso (
-    id_fonte_recurso bigserial NOT NULL,
-    codigo character(2),
-    descricao character varying(255),
-    fim_vigencia timestamp without time zone,
-    inicio_vigencia timestamp without time zone,
-    situacao character varying(10),
-    tipo character varying(20) NOT NULL,
-    CONSTRAINT mfc_fonte_recurso_situacao_check CHECK (((situacao)::text = ANY (ARRAY[('ATIVO'::character varying(10))::text, ('INATIVO'::character varying(10))::text]))),
-    CONSTRAINT mfc_fonte_recurso_tipo_check CHECK (((tipo)::text = ANY (ARRAY[('ORCAMENTARIA'::character varying(20))::text, ('ESPECIAL'::character varying(20))::text, ('EXTRAORDINARIA'::character varying(20))::text])))
-);
-
-ALTER TABLE ONLY public.fonte_recurso
-    ADD CONSTRAINT mfc_fonte_recurso_pkey PRIMARY KEY (id_fonte_recurso);
-
-
-
-INSERT INTO public.fonte_recurso
-(codigo, descricao, fim_vigencia, inicio_vigencia, situacao, tipo)
-VALUES('01', 'Tesouro', NULL, '2009-01-01 00:00:00.000', 'ATIVO', 'ORCAMENTARIA');
-INSERT INTO public.fonte_recurso
-(codigo, descricao, fim_vigencia, inicio_vigencia, situacao, tipo)
-VALUES('02', 'Transferências e Convênios Estaduais - Vinculados', NULL, '2009-01-01 00:00:00.000', 'ATIVO', 'ORCAMENTARIA');
-INSERT INTO public.fonte_recurso
-(codigo, descricao, fim_vigencia, inicio_vigencia, situacao, tipo)
-VALUES('03', 'Recursos Próprios de Fundos Especiais de Despesa - Vinculados', NULL, '2009-01-01 00:00:00.000', 'ATIVO', 'ORCAMENTARIA');
-INSERT INTO public.fonte_recurso
-(codigo, descricao, fim_vigencia, inicio_vigencia, situacao, tipo)
-VALUES('04', 'Recursos Próprios da Administração Indireta', NULL, '2009-01-01 00:00:00.000', 'ATIVO', 'ORCAMENTARIA');
-INSERT INTO public.fonte_recurso
-(codigo, descricao, fim_vigencia, inicio_vigencia, situacao, tipo)
-VALUES('05', 'Transferências e Convênios Federais - Vinculados', NULL, '2009-01-01 00:00:00.000', 'ATIVO', 'ORCAMENTARIA');
-INSERT INTO public.fonte_recurso
-(codigo, descricao, fim_vigencia, inicio_vigencia, situacao, tipo)
-VALUES('06', 'Outras Fontes de Recursos', NULL, '2009-01-01 00:00:00.000', 'ATIVO', 'ORCAMENTARIA');
-INSERT INTO public.fonte_recurso
-(codigo, descricao, fim_vigencia, inicio_vigencia, situacao, tipo)
-VALUES('07', 'Operações de Crédito', NULL, '2009-01-01 00:00:00.000', 'ATIVO', 'ORCAMENTARIA');
-INSERT INTO public.fonte_recurso
-(codigo, descricao, fim_vigencia, inicio_vigencia, situacao, tipo)
-VALUES('91', 'Tesouro - Exercícios Anteriores', NULL, '2009-01-01 00:00:00.000', 'ATIVO', 'ORCAMENTARIA');
-INSERT INTO public.fonte_recurso
-(codigo, descricao, fim_vigencia, inicio_vigencia, situacao, tipo)
-VALUES('92', 'Transferências e Convênios Estaduais - Vinculados - Exercícios Anteriores', NULL, '2009-01-01 00:00:00.000', 'ATIVO', 'ORCAMENTARIA');
-INSERT INTO public.fonte_recurso
-(codigo, descricao, fim_vigencia, inicio_vigencia, situacao, tipo)
-VALUES('93', 'Recursos Próprios de Fundos Especiais de Despesa - Vinculados - Exercícios Anteriores', NULL, '2009-01-01 00:00:00.000', 'ATIVO', 'ORCAMENTARIA');
-INSERT INTO public.fonte_recurso
-(codigo, descricao, fim_vigencia, inicio_vigencia, situacao, tipo)
-VALUES('94', 'Recursos Próprios da Administração Indireta - Exercícios Anteriores', NULL, '2009-01-01 00:00:00.000', 'ATIVO', 'ORCAMENTARIA');
-INSERT INTO public.fonte_recurso
-(codigo, descricao, fim_vigencia, inicio_vigencia, situacao, tipo)
-VALUES('95', 'Transferências e Convênios Federais - Vinculados - Exercícios Anteriores', NULL, '2009-01-01 00:00:00.000', 'ATIVO', 'ORCAMENTARIA');
-INSERT INTO public.fonte_recurso
-(codigo, descricao, fim_vigencia, inicio_vigencia, situacao, tipo)
-VALUES('96', 'Outras Fontes de Recursos - Exercícios Anteriores', NULL, '2009-01-01 00:00:00.000', 'ATIVO', 'ORCAMENTARIA');
-INSERT INTO public.fonte_recurso
-(codigo, descricao, fim_vigencia, inicio_vigencia, situacao, tipo)
-VALUES('97', 'Operações de Crédito - Exercícios Anteriores', NULL, '2009-01-01 00:00:00.000', 'ATIVO', 'ORCAMENTARIA');
-INSERT INTO public.fonte_recurso
-(codigo, descricao, fim_vigencia, inicio_vigencia, situacao, tipo)
-VALUES('98', 'Emendas Parlamentares Individuais - Exercícios Anteriores', NULL, '2009-01-01 00:00:00.000', 'ATIVO', 'ORCAMENTARIA');
-INSERT INTO public.fonte_recurso
-(codigo, descricao, fim_vigencia, inicio_vigencia, situacao, tipo)
-VALUES('08', 'Emendas Parlamentares Individuais', NULL, '2009-01-01 00:00:00.000', 'ATIVO', 'ORCAMENTARIA');
-
-
+insert into autor (id_autor, nome, sexo, email, data_nascimento, pais_origem, cpf, id_obra) values (1, 'Dan Brown', 'Masculino', 'danbrownfake@hotmail.com', '2009-03-02', 'OUTROS', '869.244.570-37', 4);
+insert into autor (id_autor, nome, sexo, email, data_nascimento, pais_origem, cpf, id_obra) values (2, 'Clarice Lispector', 'Feminino', 'clariceLispectorfake@hotmail.com', '1990-03-02', 'BRASIL', '849.121.220-50', 2);
+insert into autor (id_autor, nome, sexo, email, data_nascimento, pais_origem, cpf, id_obra) values (3, 'Homero Homem', 'Masculino', 'homeroHomemfake@hotmail.com', '1980-03-02', 'BRASIL', '280.311.500-01', 3);
+insert into autor (id_autor, nome, sexo, email, data_nascimento, pais_origem, cpf, id_obra) values (4, 'Graciliano Ramos', 'Masculino', 'graciliano_ramos@hotmail.com', '1970-03-02', 'BRASIL', '444.760.100-16', 4);
+insert into autor (id_autor, nome, sexo, email, data_nascimento, pais_origem, cpf, id_obra) values (5, 'Robert Kiyosaki', 'Masculino', 'kiyosaki_robert@hotmail.com', '1960-03-02', 'OUTROS', '872.620.450-91', 8);
 
